470d2c52c42cae7d05bcc69494e54542e7ef9790
melisa108 <kagyulife@gmail.com>
2022-09-14 23:35:33 +0200

    changed warnings in tests

--- src/test/java/com/dataart/ExampleTest.java
+++ src/test/java/com/dataart/ExampleTest.java
@@ -3,17 +3,19 @@ package com.dataart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
 
     @Test
     void exampleTest0(){
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(new Random().nextInt() > 1);
 
     }
 
     @Test
     void exampleTest1(){
-        Assertions.assertTrue(3 > 1);
+        Assertions.assertTrue(new Random().nextInt() > 2);
 
     }
 }
