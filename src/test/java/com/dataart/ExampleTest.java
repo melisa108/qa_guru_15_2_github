27f3588463c02bbd6adcca19f26312c5c7067266
melisa108 <kagyulife@gmail.com>
2022-09-14 22:43:36 +0200

    Init commit from console

---
+++ src/test/java/com/dataart/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.dataart;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest(){
+        Assertions.assertTrue(2 > 1);
+
+    }
+}
